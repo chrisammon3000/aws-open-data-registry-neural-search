18429c4c8284102c340e67eebc33b83b1302793b
Gregory Lindsey <gclindsey@gmail.com>
2022-11-29 19:06:32 -0800

    Initial commit

---
+++ test/aws-open-data-registry-neural-search.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsOpenDataRegistryNeuralSearch from '../lib/aws-open-data-registry-neural-search-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-open-data-registry-neural-search-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsOpenDataRegistryNeuralSearch.AwsOpenDataRegistryNeuralSearchStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
