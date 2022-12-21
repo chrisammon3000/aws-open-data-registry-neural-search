8f14c9ab92550f347c2d82333c06bdbfd567381d
Gregory Lindsey <gclindsey@gmail.com>
2022-12-20 22:15:46 -0800

    update weaviate image in user data

--- src/config.sh
+++ src/config.sh
@@ -8,8 +8,10 @@ newgrp docker
 pip3 install docker-compose
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
+# Minimal configuration for speed
 # curl -o docker-compose.yml "https://configuration.semi.technology/v2/docker-compose/docker-compose.yml?gpu_support=false&media_type=text&modules=modules&ner_module=false&qna_module=false&ref2vec_centroid=false&runtime=docker-compose&spellcheck_module=false&sum_module=false&text_module=text2vec-transformers&transformers_model=sentence-transformers-multi-qa-MiniLM-L6-cos-v1&weaviate_version=v1.16.5"
-# Q&A module
+# Includes Q&A module
+# TODO mount data volume for persistence
 curl -o docker-compose.yml "https://configuration.semi.technology/v2/docker-compose/docker-compose.yml?gpu_support=false&media_type=text&modules=modules&ner_module=true&ner_module_model=dbmdz-bert-large-cased-finetuned-conll03-english&qna_module=true&qna_module_model=distilbert-base-uncased-distilled-squad&ref2vec_centroid=false&runtime=docker-compose&spellcheck_module=false&sum_module=false&text_module=text2vec-transformers&transformers_model=sentence-transformers-multi-qa-MiniLM-L6-cos-v1&weaviate_version=v1.16.5"
 docker-compose up -d
-# create schema using curl
+# TODO create schema using curl
