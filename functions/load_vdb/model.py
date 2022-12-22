06f8a9d71b00d042f880b2d525406935e49b87f8
Gregory Lindsey <gclindsey@gmail.com>
2022-12-21 17:35:20 -0800

    build data objects, references are assigned as cartesian product and need correct mapping

--- functions/load_vdb/model.py
+++ functions/load_vdb/model.py
@@ -47,51 +47,25 @@ class JsonToWeaviate:
 
     def _build_objects(self):
         for map in self._mappings:
-            setattr(self.classes, map["class"], UnmarshalledJson())
             if not map["path"]:
                 expr = f"[{{{', '.join([f'{k}: {v}' for k, v in map['substitutions'].items()])}}}]"
+            elif jmespath.search(map["path"], self.input) is None:
+                # Skip if path is not in JSON
+                continue
             else:
                 expr = f"map(&{{{', '.join([f'{k}: {v}' for k, v in map['substitutions'].items()])}}}, {map['path']})"
+            setattr(self.classes, map["class"], UnmarshalledJson())
             setattr(
                 getattr(self.classes, map["class"]),
                 "data_objects",
                 self.build_weaviate_object(map["class"], expr, self.input)
             )
-            ids = [item["id"] for item in getattr(getattr(self.classes, map["class"]), "data_objects")]
 
             # set ids
-            if len(ids) == 1:
-                setattr(getattr(self.classes, map["class"]), "id", ids[0])
-            else:
-                setattr(getattr(self.classes, map["class"]), "ids", ids)
+            ids = [item["id"] for item in getattr(getattr(self.classes, map["class"]), "data_objects")]
+            setattr(getattr(self.classes, map["class"]), "ids", ids)
 
-    def _build_references(self):
-        for ref_spec in self._references:
-            setattr(getattr(self.classes, ref_spec["fromClass"]), "references", UnmarshalledJson())
-        for ref_spec in self._references:
-            # # request reference
-            # client.data_object.reference.add(
-            #     from_uuid='e067f671-1202-42c6-848b-ff4d1eb804ab',
-            #     from_property_name='wroteBooks',
-            #     to_uuid='a9c1b714-4f8a-4b01-a930-38b046d69d2d',
-            #     from_class_name='Author', # ONLY with Weaviate >= 1.14.0
-            #     to_class_name='Book', # ONLY with Weaviate >= 1.14.0
-            # )
-            if hasattr(getattr(self.classes, ref_spec["toClass"]), 'id'):
-                to_uuids = [getattr(getattr(self.classes, ref_spec["toClass"]), 'id')]
-            elif hasattr(getattr(self.classes, ref_spec["toClass"]), 'ids'):
-                to_uuids = getattr(getattr(self.classes, ref_spec["toClass"]), 'ids')
-
-            ref = [ {
-                "from_uuid": getattr(self.classes, ref_spec["fromClass"]).id,
-                "from_property_name": ref_spec["property"],
-                "to_uuid": to_uuid,
-                "from_class_name": ref_spec["fromClass"],
-                "to_class_name": ref_spec["toClass"],
-            } for to_uuid in to_uuids ]
-            setattr(getattr(getattr(self.classes, ref_spec["fromClass"]), "references"), ref_spec["property"], ref)
-        
-
+    # TODO need a way of handling arrays inside arrays: Services class is an example
     @staticmethod
     def build_weaviate_object(class_name, expr, data):
         return [
@@ -99,20 +73,71 @@ class JsonToWeaviate:
             for item in jmespath.search(expr, data)
         ]
 
+    def _build_references(self):
+        for ref_spec in self._references:
+            # Skip if class is not in JSON
+            if not hasattr(self.classes, ref_spec["fromClass"]):
+                continue
+            setattr(getattr(self.classes, ref_spec["fromClass"]), "references", UnmarshalledJson())
+        for ref_spec in self._references:
+            # Skip if class is not in JSON
+            if not hasattr(self.classes, ref_spec["toClass"]):
+                continue
+            if hasattr(getattr(self.classes, ref_spec["toClass"]), 'id'):
+                to_uuids = [getattr(getattr(self.classes, ref_spec["toClass"]), 'id')]
+            elif hasattr(getattr(self.classes, ref_spec["toClass"]), 'ids'):
+                to_uuids = getattr(getattr(self.classes, ref_spec["toClass"]), 'ids')
+
+            # TODO how to handle setting relationships to objects inside arrays? ie., Tutorials[].Services[]
+            # TODO --> try a nested list comprehension like this inside try/except block, just make sure the order is correct
+            # TODO --> could also try just keeping the ids attribute as a list so only one block is necessary
+            # ref = [ {
+            #     "from_uuid": _id,
+            #     "from_property_name": ref_spec["property"],
+            #     "to_uuid": to_uuid,
+            #     "from_class_name": ref_spec["fromClass"],
+            #     "to_class_name": ref_spec["toClass"],
+            # } for to_uuid in to_uuids for _id in getattr(self.classes, ref_spec["fromClass"]).ids]
+            ids = getattr(self.classes, ref_spec["fromClass"]).ids
+            # ref = [ {
+            #     "from_uuid": ids,
+            #     "from_property_name": ref_spec["property"],
+            #     "to_uuid": to_uuid,
+            #     "from_class_name": ref_spec["fromClass"],
+            #     "to_class_name": ref_spec["toClass"],
+            # } for to_uuid in to_uuids ]
+            print("")
+            ref = [ {
+                "from_uuid": _id,
+                "from_property_name": ref_spec["property"],
+                "to_uuid": to_uuid,
+                "from_class_name": ref_spec["fromClass"],
+                "to_class_name": ref_spec["toClass"],
+            } for _id in ids for to_uuid in to_uuids ]
+            setattr(getattr(getattr(self.classes, ref_spec["fromClass"]), "references"), ref_spec["property"], ref)
+
     @property
     def input(self):
         return self._input.__dict__
 
+    # TODO polish this
+    @property
+    def records(self):
+        return [ item.data_objects for item in self.classes.__dict__.values() ]
 
 if __name__ == "__main__":
     from pathlib import Path
 
-    # open a json file
-    path = Path(__file__).parent / "event.json"
-    with open(path) as file:
-        event = json.load(file)
+    # # open a json file
+    # path = Path(__file__).parent / "event.json"
+    # with open(path) as file:
+    #     event = json.load(file)
 
-    data = json.loads(event["Records"][0]["body"])["data"]
+    # data = json.loads(event["Records"][0]["body"])["data"]
+
+    with open('___data/output/aws-odr.json') as f:
+        data_objs = json.load(f)
+    data = data_objs[2]
 
     mappings = [
         {
@@ -148,6 +173,43 @@ if __name__ == "__main__":
                 "type": "@.Type",
             },
         },
+        {
+            "class": "Tutorial",
+            "path": "DataAtWork.Tutorials",
+            "substitutions": {
+                "title": "@.Title",
+                "url": "@.URL",
+                "authorName": "@.AuthorName",
+                "authorUrl": "@.AuthorURL",
+            },
+        },
+        {
+            "class": "Publication",
+            "path": "DataAtWork.Publications",
+            "substitutions": {
+                "title": "@.Title",
+                "url": "@.URL",
+                "authorName": "@.AuthorName",
+                "authorUrl": "@.AuthorURL",
+            },
+        },
+        {
+            "class": "ToolOrApplication",
+            "path": "DataAtWork.\"Tools & Applications\"",
+            "substitutions": {
+                "title": "@.Title",
+                "url": "@.URL",
+                "authorName": "@.AuthorName",
+                "authorUrl": "@.AuthorURL",
+            }
+        },
+        {
+            "class": "Service",
+            "path": "DataAtWork.Tutorials[].Services[]",
+            "substitutions": {
+                "name": "@"
+            }
+        }
     ]
 
     references = [
@@ -166,9 +228,27 @@ if __name__ == "__main__":
             "toClass": "Resource",
             "property": "hasResource",
         },
+        {
+            "fromClass": "Dataset",
+            "toClass": "Tutorial",
+            "property": "hasTutorial",
+        },
+        {
+            "fromClass": "Tutorial",
+            "toClass": "Service",
+            "property": "usesService",
+        },
     ]
 
-    factory = JsonToWeaviate(mappings, references)
-    builder = JsonToWeaviate.from_json(factory, data)
+    builders = []
+    for idx, data in enumerate(data_objs):
+        factory = JsonToWeaviate(mappings, references)
+        builder = JsonToWeaviate.from_json(factory, data)
+        builders.append(builder)
+        with open(f'___data/output/all_data/all-records-{idx}.json', 'w') as f:
+            json.dump(builder.records, f, indent=4)
+
+        if idx == 10:
+            break
 
     print("")
