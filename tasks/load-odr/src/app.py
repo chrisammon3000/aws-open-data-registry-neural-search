1445f646de5dec44e3af326ac7a1b85f19bff854
Gregory Lindsey <gclindsey@gmail.com>
2023-01-01 20:40:48 -0800

    load weaviate using mappings

---
+++ tasks/load-odr/src/app.py
@@ -0,0 +1,99 @@
+import os
+import logging
+from pathlib import Path
+import subprocess
+import yaml
+import json
+from jsonviate import JsonToWeaviate
+import weaviate
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+REPO_URL = os.environ["REPO_URL"]
+TARGET_DATA_DIR = os.environ["TARGET_DATA_DIR"]
+data_dir = Path(".")
+WEAVIATE_ENDPOINT = os.environ["WEAVIATE_ENDPOINT"]
+
+# configure the batch settings
+client = weaviate.Client(WEAVIATE_ENDPOINT)
+client.batch.configure(
+    batch_size=100,
+    dynamic=False,
+    timeout_retries=3,
+    callback=weaviate.util.check_batch_result,
+)
+
+
+def parse_repo_url(repo_url: str) -> str:
+    """Parse the repo URL and return the owner and repository name."""
+    owner, repo = repo_url.split("/")[-2:]
+    return owner, repo
+
+
+def load_target_files(target_data_path: Path) -> list:
+    """Yields yaml files from the target data path."""
+    for file in target_data_path.iterdir():
+        if file.suffix in [".yaml", ".yml"]:
+            with open(file, "r") as f:
+                yield yaml.safe_load(f)
+
+
+def load_spec(spec_path: Path) -> dict:
+    """Load JSON from the spec path."""
+    with open(spec_path, "r") as f:
+        return json.load(f)
+
+
+if __name__ == "__main__":
+    # get the owner and repo name
+    owner, repo = parse_repo_url(REPO_URL)
+    clone_target_path = data_dir / "data" / owner / repo
+    target_data_path = data_dir / "data" / owner / repo / TARGET_DATA_DIR
+
+    logger.info(f"Cloning {REPO_URL} to {clone_target_path}...")
+
+    # clone github repo from REPO_URL
+    subprocess.run(["git", "clone", REPO_URL, str(clone_target_path)])
+
+    # load yaml files as json from the target data path
+    files = load_target_files(target_data_path=target_data_path)
+
+    # create data objects
+    mappings_spec_path = Path(__file__).parent / "mappings-spec.json"
+    references_spec_path = Path(__file__).parent / "references-spec.json"
+
+    factory = JsonToWeaviate(
+        mappings=load_spec(mappings_spec_path),
+        references=load_spec(references_spec_path),
+    )
+
+    logger.info(f"Loading data objects and cross references...")
+    with client.batch as batch:
+        for idx, file in enumerate(files):
+            try:
+                # while client.batch.shape
+                mapper = JsonToWeaviate.from_json(factory, file)
+
+                
+
+                # add data objects
+                for data_object in mapper.data_objects:
+                    batch.add_data_object(
+                        data_object["data"],
+                        class_name=data_object["class"],
+                        uuid=data_object["id"],
+                    )
+
+                # add cross references
+                for cross_reference in mapper.cross_references:
+                    batch.add_reference(
+                        from_object_uuid=cross_reference['from_uuid'],
+                        from_object_class_name=cross_reference['from_class_name'],
+                        from_property_name=cross_reference['from_property_name'],
+                        to_object_uuid=cross_reference['to_uuid'],
+                        to_object_class_name=cross_reference['to_class_name'],
+                    )
+            except Exception as e:
+                logger.error(f"Error while processing file {file}: {e}")
+                continue
--- tasks/load-odr/src/jsonviate/model.py
+++ tasks/load-odr/src/jsonviate/model.py
@@ -1,12 +1,18 @@
-from dataclasses import dataclass
 import json
-import jmespath
+import logging
+from dataclasses import dataclass
+from functools import cached_property
+from itertools import chain
 from uuid import uuid4
+
+import jmespath
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 @dataclass
-class UnmarshalledJson:
-
+class JsonModel:
     __slots__ = "__dict__"
 
     def __init__(self, **kwargs):
@@ -17,39 +23,36 @@ class UnmarshalledJson:
         return json.dumps(self.__dict__)
 
     def __repr__(self) -> str:
-        pass
+        return f"{self.__class__.__name__}({self.__dict__})"
 
 
 # Automatically assembles models from json mappings but needs to use jmespath
 class JsonToWeaviate:
-    def __init__(
-        self, mappings: dict, references: dict = None, data: dict = None
-    ) -> None:
-        self._input = UnmarshalledJson(**data) if data else None
-        self._mappings = mappings
-        self._references = references
+    def __init__(self, mappings: dict, references: dict = None, data: dict = None) -> None:
+        self._input = JsonModel(**data) if data else None
+        self._class_mappings = mappings
+        self._ref_mappings = references
 
         if self._input is not None:
-            self._build()
+            self.build()
 
     @classmethod
     def from_json(cls, obj, data: str, mappings: str = None, references: str = None):
         return cls(
-            mappings=mappings or obj._mappings,
-            references=references or obj._references,
+            mappings=mappings or obj._class_mappings,
+            references=references or obj._ref_mappings,
             data=data,
         )
 
-    def _build(self):
-        self.classes = UnmarshalledJson()
+    def build(self):
+        self.classes = JsonModel()
         self._build_objects()
 
-        if self._references is not None:
+        if self._ref_mappings is not None:
             self._build_references()
 
     def _build_objects(self):
-        for map in self._mappings:
-
+        for map in self._class_mappings:
             # build search expressions
             if not map["path"]:
                 expr = f"[{{{', '.join([f'{k}: {v}' for k, v in map['substitutions'].items()])}}}]"
@@ -65,62 +68,53 @@ class JsonToWeaviate:
                     par_align = self.get_parent_alignment(map["path"])
 
             # build class
-            setattr(self.classes, map["class"], UnmarshalledJson())
+            setattr(self.classes, map["class"], JsonModel())
+            getattr(self.classes, map["class"]).name = map["class"]
+            getattr(self.classes, map["class"]).references = None
 
             # build data objects
             data_objects = self.build_weaviate_object(map["class"], expr, self.input)
-            setattr(getattr(self.classes, map["class"]), "data_objects", data_objects)
+            getattr(self.classes, map["class"]).data_objects = data_objects
 
             # set ids
-            ids = [
-                item["id"]
-                for item in getattr(getattr(self.classes, map["class"]), "data_objects")
-            ]
-            setattr(getattr(self.classes, map["class"]), "ids", ids)
+            ids = [item["id"] for item in getattr(self.classes, map["class"]).data_objects]
+            getattr(self.classes, map["class"]).ids = ids
 
             # set parent alignments
             if par_align is not None:
-                setattr(getattr(self.classes, map["class"]), "_parent_align", { align_idx: _id for align_idx, _id in zip(par_align, ids) })
-
+                getattr(self.classes, map["class"])._parent_align = {
+                    align_idx: _id for align_idx, _id in zip(par_align, ids)
+                }
 
     def get_parent_alignment(self, path):
         par_path = ".".join(path.split(".")[:-1])
         child_key = path.split(".")[-1].replace("[]", "")
-        align_map = [
-            child_key in item.keys() for item in jmespath.search(par_path, self.input)
-        ]
+        align_map = [child_key in item for item in jmespath.search(par_path, self.input)]
         return [idx for idx, i in enumerate(align_map) if i]
 
     # TODO need a way of handling arrays inside arrays: Services class is an example
     @staticmethod
     def build_weaviate_object(class_name, expr, data):
-        return [
-            {"id": str(uuid4()), "class": class_name, "data": item}
-            for item in jmespath.search(expr, data)
-        ]
+        return [{"id": str(uuid4()), "class": class_name, "data": item} for item in jmespath.search(expr, data)]
 
     def _build_references(self):
-        for ref_spec in self._references:
+        for ref_spec in self._ref_mappings:
             # Skip if class is not in JSON
             if not hasattr(self.classes, ref_spec["fromClass"]):
                 continue
-            setattr(
-                getattr(self.classes, ref_spec["fromClass"]),
-                "references",
-                UnmarshalledJson(),
-            )
-        for ref_spec in self._references:
+            getattr(self.classes, ref_spec["fromClass"]).references = JsonModel()
+        for ref_spec in self._ref_mappings:
             # Skip if class is not in JSON
             if not hasattr(self.classes, ref_spec["toClass"]):
                 continue
 
-            from_uuids = getattr(getattr(self.classes, ref_spec["fromClass"]), "ids")
-            to_uuids = getattr(getattr(self.classes, ref_spec["toClass"]), "ids")
+            from_uuids = getattr(self.classes, ref_spec["fromClass"]).ids
+            to_uuids = getattr(self.classes, ref_spec["toClass"]).ids
 
             # get alignments
             if hasattr(getattr(self.classes, ref_spec["toClass"]), "_parent_align"):
-                to_class_align_idx = getattr(getattr(self.classes, ref_spec["toClass"]), "_parent_align")
-                from_to_align_map = { from_uuids[k]: v for k, v in to_class_align_idx.items() }
+                to_class_align_idx = getattr(self.classes, ref_spec["toClass"])._parent_align
+                from_to_align_map = {from_uuids[k]: v for k, v in to_class_align_idx.items()}
 
                 # match from and to uuids based on index position
                 ref = [
@@ -130,11 +124,14 @@ class JsonToWeaviate:
                         "to_uuid": to_uuid,
                         "from_class_name": ref_spec["fromClass"],
                         "to_class_name": ref_spec["toClass"],
-                    } for from_uuid, to_uuid in from_to_align_map.items()
+                    }
+                    for from_uuid, to_uuid in from_to_align_map.items()
                 ]
             else:
                 # TODO nested list comprehension results in a cartesion product, but so far it's
                 # TODO only been tested with len(from_uuids) == 1. May want to test n:n relationships and possibly optimized this for specific conditions: 1:1, 1:n, n:1, n:n
+                if len(from_uuids) > 1:
+                    logger.warning(f"Multiple UUIDs in source class while setting references: {ref_spec['fromClass']}.")
                 ref = [
                     {
                         "from_uuid": from_uuid,
@@ -142,11 +139,13 @@ class JsonToWeaviate:
                         "to_uuid": to_uuid,
                         "from_class_name": ref_spec["fromClass"],
                         "to_class_name": ref_spec["toClass"],
-                    } for from_uuid in from_uuids for to_uuid in to_uuids
+                    }
+                    for from_uuid in from_uuids
+                    for to_uuid in to_uuids
                 ]
 
             setattr(
-                getattr(getattr(self.classes, ref_spec["fromClass"]), "references"),
+                getattr(self.classes, ref_spec["fromClass"]).references,
                 ref_spec["property"],
                 ref,
             )
@@ -155,25 +154,52 @@ class JsonToWeaviate:
     def input(self):
         return self._input.__dict__
 
-    # TODO polish this
-    @property
-    def records(self):
-        return [item.data_objects for item in self.classes.__dict__.values()]
+    @cached_property
+    def data(self):
+        return [*self.data_objects, *self.cross_references]
+
+    @cached_property
+    def data_objects(self):
+        return list(chain.from_iterable([item.data_objects for item in self.classes.__dict__.values()]))
+
+    @cached_property
+    def cross_references(self):
+        return list(
+            chain.from_iterable(
+                [
+                    data
+                    for item in self.classes.__dict__.values()
+                    if item.references is not None
+                    for data in item.references.__dict__.values()
+                ]
+            )
+        )
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}()"
+
+    # TODO Test with different types of JSON
+    # TODO Documentation of class & ref mappings
+    # TODO skip renaming and use default field names in class mapping
+    # TODO validate the ids set correctly for references
 
 
 if __name__ == "__main__":
-    from pathlib import Path
+    # from pathlib import Path
 
     # # open a json file
     # path = Path(__file__).parent / "event.json"
     # with open(path) as file:
     #     event = json.load(file)
-
     # data = json.loads(event["Records"][0]["body"])["data"]
 
-    with open("___data/output/aws-odr.json") as f:
+    with open("___data/awslabs/output/aws-odr.json") as f:
         data_objs = json.load(f)
     data = data_objs[2]
+
+    # # write to file
+    # with open("tests/data/raw/aws-odr.json", "w") as f:
+    #     json.dump(data_objs[:20], f, indent=4)
 
     mappings = [
         {
@@ -274,15 +300,29 @@ if __name__ == "__main__":
         },
     ]
 
+    # write out json files for mapping
+    path = "./tests/data"
+    with open(f"{path}/mappings.json", "w") as f:
+        json.dump(mappings, f, indent=4)
+    with open(f"{path}/references.json", "w") as f:
+        json.dump(references, f, indent=4)
+
     builders = []
+    data_objects = []
+    cross_references = []
+    factory = JsonToWeaviate(mappings, references)
     for idx, data in enumerate(data_objs):
-        factory = JsonToWeaviate(mappings, references)
         builder = JsonToWeaviate.from_json(factory, data)
         builders.append(builder)
-        with open(f"___data/output/all_data/all-records-{idx}.json", "w") as f:
-            json.dump(builder.records, f, indent=4)
-
-        if idx == 10:
+        data_objects.append(builder.data_objects)
+        cross_references.append(builder.cross_references)
+        print(builder)
+        if idx == 20:
             break
+
+    # with open(f"___data/output/all_data/data-objects.json", "w") as f:
+    #     json.dump(data_objects, f, indent=4)
+    # with open(f"___data/output/all_data/cross-references.json", "w") as f:
+    #     json.dump(cross_references, f, indent=4)
 
     print("")
